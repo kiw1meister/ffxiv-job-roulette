d3f4211205052717195b1c0fd7a98338ad8ce2a4
kiw1meister <linandy719@gmail.com>
2024-11-18 21:56:30 +0900

    chore: formatting

--- src/components/SpinWheel.vue
+++ src/components/SpinWheel.vue
@@ -1,3 +1,40 @@
+<script setup lang="ts">
+    import { ref } from 'vue'
+    import { ffxivJobs } from '../assets/Jobs';
+    import FortuneWheel from 'vue-fortune-wheel'
+    import 'vue-fortune-wheel/style.css'
+
+    interface Job {
+        id: number,
+        name: string,
+        role: string,
+        icon: string,
+        pic: string,
+        bgColor: string,
+        color: string,
+        probability: number
+    }
+  
+    const canvasVerify = ref(false) // Whether the turntable in canvas mode is enabled for verification
+    const canvasOptions = {
+        btnWidth: 140,
+        btnText: "Spin!",
+        textDirection: 'vertical',
+        borderColor: '#584b43',
+        borderWidth: 6,
+        lineHeight: 1,
+        fontSize: 20,
+        textLength: 20,
+        textRadius: 235,
+    }
+  
+    const jobs = ffxivJobs;
+  
+    function onRotateEnd (job: Job) {
+        alert("You should play " + job.name + "!")
+    }
+</script>
+
 <template>
     <div class="flex items-center justify-center">
       <FortuneWheel
@@ -9,41 +46,4 @@
         @rotateEnd="onRotateEnd"
       />
     </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref } from 'vue'
-  import { ffxivJobs } from '../assets/Jobs';
-  import FortuneWheel from 'vue-fortune-wheel'
-  import 'vue-fortune-wheel/style.css'
-
-  interface Job {
-    id: number,
-    name: string,
-    role: string,
-    icon: string,
-    pic: string,
-    bgColor: string,
-    color: string,
-    probability: number
-} 
-  
-  const canvasVerify = ref(false) // Whether the turntable in canvas mode is enabled for verification
-  const canvasOptions = {
-    btnWidth: 140,
-    btnText: "Spin!",
-    textDirection: 'vertical',
-    borderColor: '#584b43',
-    borderWidth: 6,
-    lineHeight: 1,
-    fontSize: 20,
-    textLength: 20,
-    textRadius: 235,
-  }
-  
-  const jobs = ffxivJobs;
-  
-  function onRotateEnd (job: Job) {
-    alert("You should play " + job.name + "!")
-  }
-  </script>
+</template>
